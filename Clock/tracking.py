37b1c9eb973fe3c46143a77d00bee145b8dee444
Alex <41775101+Celis1@users.noreply.github.com>
2023-09-29 22:57:36 -0400

    working clock class

--- Clock/tracking.py
+++ Clock/tracking.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta, time
+import calendar
+
 from time import sleep
 import pickle
 import os
@@ -37,7 +39,10 @@ class TrackingClock:
         deadline: string of the deadline in the format of 'mm/dd/yyyy'
         '''
         deadline += ' 09:00:00'
-        parsed_date = datetime.strptime(deadline, "%m/%d/%Y %H:%M:%S")
+        deadline_date = datetime.strptime(deadline, "%m/%d/%Y %H:%M:%S")
+        deadline_created = datetime.now()
+        # TODO : not the best way to store this as a tuple
+        parsed_date = (deadline_created, deadline_date)
 
         # TODO : might need to make this modular
         with open("./DATA/TimeData/deadline.pkl", "wb") as file:
@@ -74,11 +79,17 @@ class TrackingClock:
 
     @staticmethod
     def get_time():
+        '''
+        Function for getting the current time and displaying it properly
+        '''
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime(TrackingClock.TIME_FORMAT)
         return formatted_datetime
 
     def normalize(self, start_time, current_time, end_time):
+        '''
+        Function for normalizing the time to a value between 0 and 1
+        '''
 
         # Calculate the total time range as a timedelta
         total_time_range = end_time - start_time
@@ -94,19 +105,75 @@ class TrackingClock:
         
         return normalized_value
         
-    def initialize_progress(self):
-        pass
 
     def deadline_progress(self):
-        pass
+        '''
+        Function for getting the progress of the current date to the deadline
+        '''
+        start_date, end_date = self.get_deadline()
+        now = datetime.now()
+
+        # Calculate the normalized value
+        norm_value = self.normalize(start_date, now, end_date)
+
+        # calculate the days left
+        days_left = (end_date - now).days
+
+        return norm_value, days_left
+
 
     def year_progress(self):
-        pass
+        '''
+        Function for getting the progress of the current year starting from the 1st of
+        January to the 31st of December
+        '''
+        # Get the current date
+        now = datetime.now()
+
+        # Calculate the total number of days in the current year
+        number_of_days = 365 if now.year % 4 else 366
+
+        # get the current day
+        current_day = now.timetuple().tm_yday
+
+        # create a datetime object for the first day of the year
+        first_day = now.replace(month=1, day=1, hour=0, minute=0, second=0)
+        last_day = now.replace(month=12, day=31, hour=23, minute=59, second=59)
+
+        # Calculate the normalized value
+        days_left = number_of_days - current_day
+        norm_value = self.normalize(first_day, now, last_day)
+
+        return norm_value, days_left
 
     def month_progress(self):
-        pass
+        '''
+        Function for getting the progress of the current month starting from the 1st of
+        the current month to the last day
+        '''
+        # Get the current date
+        now = datetime.now()
+
+        # Calculate the total number of days in the current month
+        number_of_days = calendar.monthrange(now.year, now.month)[1]
+
+        # get the current day
+        current_day = now.day
+
+        # create a datetime object for the first day of the month
+        first_day = now.replace(day=1, hour=0, minute=0, second=0)
+        last_day = now.replace(day=number_of_days, hour=23, minute=59, second=59)
+        print('debugging',number_of_days, first_day, now, last_day)
+
+        # Calculate the normalized value
+        days_left = number_of_days - current_day
+        norm_value = self.normalize(first_day, now, last_day)
+
+
+        return norm_value, days_left
 
     def get_day_progress(self):
+        # TODO : normalization is correct but the amount of hours left in the day is not!
         # Get the current date and time
         now = datetime.now()
 
@@ -129,7 +196,7 @@ class TrackingClock:
         yesterday_9am = datetime(yesterday.year, yesterday.month, yesterday.day, 9, 0, 0)
         
         # Calculate the time difference in hours
-        time_difference = now.hour - yesterday_9am.hour
+        time_difference = yesterday_9am.hour - now.hour
         
         #normalizing the time for progress bar
         norm_time = self.normalize(yesterday_9am, now, tomorrow)
@@ -144,10 +211,14 @@ if __name__ == "__main__":
     date_string = "7/23/2025"
     clock.create_deadline(date_string)
 
+    print('deadline')
     print(clock.get_deadline())
 
 
     while True:
         print(clock.get_time())
         print(clock.get_day_progress())
+        print(clock.month_progress())
+        print(clock.year_progress())
+        print(clock.deadline_progress())
         sleep(1)
