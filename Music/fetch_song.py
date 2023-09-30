e2ccc6e347be10cb542022a147d977bd6000ca4b
Alex <41775101+Celis1@users.noreply.github.com>
2023-09-29 23:44:28 -0400

    allowing saving songs

--- Music/fetch_song.py
+++ Music/fetch_song.py
@@ -5,17 +5,14 @@ import vlc
 
 class MusicStreamer:
     def __init__(self) -> None:
-        pass
+        self.music_path = './DATA/MusicData'
 
     def play_song(self, youtube_url, start_time = 0):
         audio_url, info = self.load_song(youtube_url)
-        player = self.create_vlc(audio_url)
-
+        
         # Initialize the media player
+        player = self.create_vlc(audio_url)
         player.play()
-
-        # Wait for the player to start
-        # time.sleep(1)
 
         # Set the start time to 1 minute (60 seconds)
         if start_time != 0:
@@ -59,9 +56,16 @@ class MusicStreamer:
         player.set_media(Media)
         return player
 
+    def download_song(self, youtube_url):
+        # Download the video using Pytube
+        yt = pytube.YouTube(youtube_url)
+        video_stream = yt.streams.filter(file_extension='mp4').first()
+        video_stream.download(self.music_path)
+        return True
 
 
 
 if __name__ == "__main__":
     streamer = MusicStreamer()
-    streamer.play_song('https://www.youtube.com/watch?v=YU-m46on_bQ', 130)
+    # streamer.play_song('https://www.youtube.com/watch?v=YU-m46on_bQ', 130)
+    streamer.download_song('https://www.youtube.com/watch?v=YU-m46on_bQ')
