b91f45efe0d8266ef3ead29324c277454030a9de
Alex <41775101+Celis1@users.noreply.github.com>
2023-09-30 04:18:45 -0400

    working app

--- GUI/basic_app.py
+++ GUI/basic_app.py
@@ -2,35 +2,50 @@ import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
 
-def start_loading():
-    loading_bar.start(10)  # Start the loading animation
-    root.after(3000, stop_loading)  # Simulate loading for 3 seconds
+# Function to update the clock display
+def update_clock():
+    curr_time = datetime.now().strftime('%H:%M:%S')
+    clock_label.config(text=curr_time)
+    root.after(1000, update_clock)
 
-def stop_loading():
-    loading_bar.stop()  # Stop the loading animation
-
-def update_datetime():
-    current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%I:%M:%S %p\n%A, %B %d, %Y")
-    datetime_label.config(text="Current Date and Time:\n" + formatted_datetime)
-    root.after(1000, update_datetime)  # Update the datetime every 1 second
-
+# Initialize the Tkinter window
 root = tk.Tk()
-root.title("Date and Time Display App")
-root.geometry("400x200")
-root.configure(bg="black")  # Set the default background color to black
+root.title("Digital Clock with Progress Bars")
 
-datetime_label = tk.Label(root, text="Current Date and Time: 00:00:00 AM\nDay, Month Day, Year",
-                          font=("Helvetica", 14), fg="white", justify="center")
-datetime_label.pack(pady=20)
+# Configure the dark mode theme
+root.configure(bg='#1e1e1e')  # Set background color to dark gray
+ttk.Style().configure('TLabel', background='#1e1e1e', foreground='#ffffff')  # Set label text color to white
+ttk.Style().configure('TFrame', background='#1e1e1e')  # Set frame background color to dark gray
+ttk.Style().configure('TButton', background='#444444', foreground='#ffffff')  # Set button colors
 
-loading_frame = ttk.Frame(root)
-loading_frame.pack()
+# Create a label for displaying the time
+clock_label = ttk.Label(root, font=('Helvetica', 48))
+clock_label.pack(pady=20)  # Add some padding to center the time label
 
-loading_bar = ttk.Progressbar(loading_frame, mode="indeterminate", length=200)
-loading_bar.grid(row=0, column=0, padx=10, pady=10)
+# Create a frame for the progress bars
+progress_frame = ttk.Frame(root)
+progress_frame.pack(pady=20)
 
-start_loading()  # Start loading when the app is launched
-update_datetime()  # Update the datetime label
+# Create progress bars
+progress_labels = ['Day Progress', 'Month Progress', 'Year Progress', 'Deadline Progress']
+progress_bars = []
 
+for label in progress_labels:
+    ttk.Label(progress_frame, text=label).pack()
+    progress_bar = ttk.Progressbar(progress_frame, orient='horizontal', length=300, mode='determinate')
+    progress_bar.pack(pady=5)
+    progress_bars.append(progress_bar)
+
+# Function to update progress bars
+def update_progress_bars():
+    for i, label in enumerate(progress_labels):
+        progress_bars[i]['value'] = info[label.lower().replace(' ', '_')]
+        progress_bars[i].update()
+    root.after(60000, update_progress_bars)  # Update progress bars every minute
+
+# Call the update_clock and update_progress_bars functions to display time and progress
+update_clock()
+update_progress_bars()
+
+# Run the Tkinter main loop
 root.mainloop()
