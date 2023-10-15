3ed95e288a52872641531b379abcbfcc2b2aa574
Alex <41775101+Celis1@users.noreply.github.com>
2023-10-15 05:50:24 -0400

    added old functions to new structure

--- GUI/Screens/alarm_screen.py
+++ GUI/Screens/alarm_screen.py
@@ -6,5 +6,5 @@ from ttkbootstrap.constants import *
 class AlarmScreen(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
-        self.label = ttk.Label(self, text="Page 2")
-        self.label.pack(fill=tk.BOTH, expand=True)
+        # self.label = ttk.Label(self, text="Page 2")
+        # self.label.pack(fill=tk.BOTH, expand=True)
